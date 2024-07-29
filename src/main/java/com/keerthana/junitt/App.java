81bf6f0178febecf0e8b75b96da2ab746a3c587a
Keerthanaa <keerthy16@gmail.com>
2024-07-29 10:50:35 +0530

    code smell2

--- src/main/java/com/keerthana/junitt/App.java
+++ src/main/java/com/keerthana/junitt/App.java
@@ -1,4 +1,4 @@
-package com.keerthana.junit;
+package com.keerthana.junitt;
 
 /**
  * Hello world!
--- src/test/java/com/keerthana/junittest/AppTest.java
+++ src/test/java/com/keerthana/junittest/AppTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import com.keerthana.junit.App;
+import com.keerthana.junitt.App;
 
 /**
  * 
